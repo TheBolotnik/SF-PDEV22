2aefd4c8d49d0c2c85ce6a34cf49c88b80f7fb18
TheBolotnik <124118625+TheBolotnik@users.noreply.github.com>
2023-04-20 13:16:39 +0500

    SimpleApp

---
+++ project/simpleapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView
+from .models import Product
+
+
+class ProductsList(ListView):
+       model = Product
+       ordering = 'name'
+       template_name = 'products.html'
+       context_object_name = 'products'
+# Create your views here.
