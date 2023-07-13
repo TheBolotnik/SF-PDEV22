0596038b2221a9eb8e7c9e930f2a4c4fe3a7d6f8
TheBolotnik <124118625+TheBolotnik@users.noreply.github.com>
2023-07-13 14:58:16 +0500

    Add files via upload

--- NewsPapper/news/forms.py
+++ NewsPapper/news/forms.py
@@ -71,6 +71,13 @@ class UserSignUpForm(SignupForm):
         basic_group.user_set.add(user)
         return user
 
+class CommonSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(CommonSignupForm, self).save(request)
+        common_group = Group.objects.get(name='common')
+        common_group.user_set.add(user)
+        return user
 
 
 class LogInForm(AuthenticationForm):
--- NewsPapper/news/urls.py
+++ NewsPapper/news/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.urls import path, include
 from .views import *
-import allauth
+
 
 urlpatterns = [
     path('', NewsHome.as_view(), name='home'),
