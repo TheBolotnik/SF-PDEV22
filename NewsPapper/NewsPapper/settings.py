585046b6812f81fbd250326c915f3f2a8b4a1a9b
TheBolotnik <124118625+TheBolotnik@users.noreply.github.com>
2023-11-14 17:05:05 +0500

    Add files via upload

--- NewsPapper/NewsPapper/settings.py
+++ NewsPapper/NewsPapper/settings.py
@@ -168,16 +168,16 @@ STATICFILES_DIRS = []
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DEFAULT_FROM_EMAIL = 'SF-PDEV-27'
+DEFAULT_FROM_EMAIL = '_'
 
-EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
+EMAIL_HOST = '_'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
-EMAIL_HOST_USER = 'SF-PDEV-27'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = '123'  # пароль от почты
+EMAIL_HOST_USER = '_'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
+EMAIL_HOST_PASSWORD = '_'  # пароль от почты
 EMAIL_USE_SSL = True  # Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
 
-SMTP_USER = 'TheBolotnik@gmail.com'
-SMTP_PASSWORD = 'qdgk pqeb ztfo nwcs'
+SMTP_USER = '_'
+SMTP_PASSWORD = '_'
 
 
 if DEBUG:
