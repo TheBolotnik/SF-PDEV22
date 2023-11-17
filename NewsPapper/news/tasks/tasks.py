c19d9e4731a553208d6a204e8382c5ba73ad3e44
TheBolotnik <124118625+TheBolotnik@users.noreply.github.com>
2023-11-17 12:17:13 +0500

    Add files via upload

--- NewsPapper/news/tasks/tasks.py
+++ NewsPapper/news/tasks/tasks.py
@@ -1,50 +1,32 @@
 import smtplib
-
+from NewsPapper.news.models import *
+from NewsPapper.news.signals import notify_news_subscriber, send_notifications
+from django.http import HttpResponse
+from django.views import View
 from celery import Celery, shared_task
-from email.message import EmailMessage
-from NewsPapper.NewsPapper.settings import SMTP_USER, SMTP_PASSWORD
-import time
+from celery.schedules import crontab
 
-
-SMTP_HOST = 'smtp.gmail.com'
-SMTP_PORT = 465
-
-
-# def get_email_template(username: str):
-#     email = EmailMessage()
-#     email['Subject'] = 'Новостной портал'
-#     email['From'] = SMTP_USER
-#     email['To'] = SMTP_USER
-#
-#     email.set_content(
-#         '<div>'
-#         f'<h1>Здравствуйте {username}, вы пропустили следующие <a href="/home">новости:</a></h1>'
-#         '</div>',
-#         subtype='html'
-#     )
-#     return email
-#
-#
-# def send_email_news(username: str):
-#     email = get_email_template(username)
-#     with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as server:
-#         server.login(SMTP_USER, SMTP_PASSWORD)
-#         server.send_message(email)
+@shared_task
+def weekly_news():
+    send_news = notify_news_subscriber
+    return send_news
 
 
 @shared_task
 def hello():
-    time.sleep(10)
+    time.sleep(2)
     print('Hello World!')
 
 
+@shared_task
 def printer(N):
     for i in range(N):
         time.sleep(1)
         print(i+1)
 
-# def get(self, request):
-#     printer.delay(10)
-#     hello.delay()
-#     from django.http import HttpResponse
-#     return HttpResponse('Hello!')
+
+class MyTask(View):
+    def get(self, request):
+        printer.delay(10)
+        hello.delay()
+        return HttpResponse('Hello world!')
